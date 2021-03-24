bf11a83e2610bfdf6495606e3c13703372e8d208
david.wang[王稔太] <david.wang@davidwang-mac13-m1.local>
2021-03-24 10:17:16 +0800

    test

--- Psr/logger.php
+++ Psr/logger.php
@@ -3,14 +3,21 @@
 namespace Php\Exam;
 use Psr\Log\LoggerInterface;
 
-class Logger
+class Logger implements LoggerInterface
 {
     function __construct(){
-        echo "!!!!!!!!!!!!@@@@@@@@@@@@".PHP_EOL;
+        echo "info".PHP_EOL;
+        echo "notice".PHP_EOL;
+        echo "notice".PHP_EOL;
+        echo "critical".PHP_EOL;
+        echo "info".PHP_EOL;
+        echo "error".PHP_EOL;
+        echo "notice".PHP_EOL;
+    }
+
+    public function debug($message, array $context = array() ) {
+        echo "notice".PHP_EOL;
     }
 }
-
-
-
 
 ?>
