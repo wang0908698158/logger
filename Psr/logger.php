d76ce094a9b7e1b44d141d6edb480478935b9314
david.wang[王稔太] <david.wang@davidwang-mac13-m1.local>
2021-03-24 10:30:50 +0800

    interface

--- Psr/logger.php
+++ Psr/logger.php
@@ -15,7 +15,42 @@ class Logger implements LoggerInterface
         echo "notice".PHP_EOL;
     }
 
-    
+    public function emergency($message, array $context = array()) {
+        
+    }
+
+    public function alert($message, array $context = array()) {
+
+    }
+
+    public function critical($message, array $context = array()) {
+
+    }
+
+    public function error($message, array $context = array()) {
+
+    }
+
+    public function warning($message, array $context = array()) {
+
+    }
+
+    public function notice($message, array $context = array()) {
+
+    }
+
+    public function info($message, array $context = array()) {
+
+    }
+
+    public function debug($message, array $context = array()) {
+
+    }
+
+    public function log($level, $message, array $context = array()) {
+
+    }
+
 }
 
 ?>
